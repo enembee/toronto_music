01a706386e7db04b4c1bfd356ca0adbe065e66e8
enembee <nickbownes@hotmail.com>
2013-09-03 15:47:30 -0400

    Genre and Location pages
    Genre and location admin screens

---
+++ add_genre.php
@@ -0,0 +1,37 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	require_once("../db_connect.php");
+
+	$errors = array();
+
+	if (empty($_POST["genre_name"])) {
+		$errors[] = "Please enter a genre name";
+	} else {
+		$gn = mysqli_real_escape_string($dbc, trim($_POST["genre_name"]));
+	}
+
+	if (empty($errors)) {
+		$q = "INSERT INTO genres (genre_name) VALUES ('$gn')";
+		$r = mysqli_query($dbc, $q);
+		if ($r) {
+			echo "Success! Genre added.";
+		} else {
+			echo mysqli_error($dbc);
+		}
+
+		mysqli_close($dbc);
+	} else {
+		foreach ($errors as $msg) {
+			echo "- $msg<br />\n";
+		}
+	}
+}
+
+echo '<form action="add_genre.php" method="post">
+		<p>Genre Name:<input type="text" name="genre_name" size="20" maxlength="40" /></p>
+		<p><input type="submit" name="submit" value="Add Genre" /></p>
+	  </form>';
+?>
+
+</body>
+</html>
---
+++ add_location.php
@@ -0,0 +1,66 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	require_once("../db_connect.php");
+
+	$errors = array();
+
+	if (empty($_POST["location_name"])) {
+		$errors[] = "Please enter a location name";
+	} else {
+		$ln = mysqli_real_escape_string($dbc, trim($_POST["location_name"]));
+	}
+
+	if (empty($_POST["address_1"])) {
+		$errors[] = "Please enter the first line of the address";
+	} else {
+		$a1 = mysqli_real_escape_string($dbc, trim($_POST["address_1"]));
+	}
+
+	if (!empty($a2)) {
+		$a2 = mysqli_real_escape_string($dbc, trim($_POST["address_2"]));
+	} else {
+			$a2 = NULL;
+	}
+
+	if (empty($_POST["city"])) {
+		$errors[] = "Please enter the name of the city";
+	} else {
+		$c = mysqli_real_escape_string($dbc, trim($_POST["city"]));
+	}
+
+	if (empty($_POST["postal_code"])) {
+		$errors[] = "Please enter the postal code";
+	} else {
+		$pc = mysqli_real_escape_string($dbc, trim($_POST["postal_code"]));
+	}
+
+
+	if (empty($errors)) {
+		$q = "INSERT INTO locations (location_name, address_1, address_2, city, postal_code) VALUES ('$ln', '$a1', '$a2', '$c', '$pc')";
+		$r = mysqli_query($dbc, $q);
+		if ($r) {
+			echo "Success! Location added.";
+		} else {
+			echo mysqli_error($dbc);
+		}
+
+		mysqli_close($dbc);
+	} else {
+		foreach ($errors as $msg) {
+			echo "- $msg<br />\n";
+		}
+	}
+}
+
+echo '<form action="add_location.php" method="post">
+		<p>Venue Name:<input type="text" name="location_name" size="20" maxlength="40" /></p>
+		<p>Address Line 1:<input type="text" name="address_1" size="20" maxlength="40" /></p>
+		<p>Address Line 2:<input type="text" name="address_2" size="20" maxlength="40" /></p>
+		<p>City:<input type="text" name="city" size="20" maxlength="20" /></p>
+		<p>Postal Code:<input type="text" name="postal_code" size="20" maxlength="10" /></p>
+		<p><input type="submit" name="submit" value="Add Location" /></p>
+	  </form>';
+?>
+
+</body>
+</html>
